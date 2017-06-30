5ba36ce2284c8f9e9accc33e95fa72557a0bc667
Patricia <patricia.santana@metasix.com.br>
2017-06-30 14:37:59 -0300

    Refatorando a rota de busca por id

--- src/routes/serie.js
+++ src/routes/serie.js
@@ -21,31 +21,33 @@ router.get('/series', function (req, res) {
 });
 
 router.get('/series/:id', function (req, res) {
-	let numero = 0; i
-	let tem = false;
-	numero = req.params.id;
-	for (let linha in listaSeries) {
-		if (listaSeries[linha].id == numero) {
-			res.send(listaSeries[linha]);
-			tem = true;
-		}
-	}
-	if (tem == false) {
-		res.status(404).send({ error: 'Id não encontrado' });
-	}
+	let novalista = [];
+	let idSerie = req.params.id;
+	
+	novalista = listaSeries.filter(function (novalista) {
+		return novalista.id == idSerie;
+	});
+
+	if (novalista.length > 0)
+		res.send(novalista);
+	else
+		res.status(404).send({ error: 'Id não encontrado' });	
 });
 
 
 router.post('/series', function (req, res) {
-	let serie = req.body;
-	let tamanho = listaSeries.length;
+	let novaSerie = req.body;
+	novalista = listaSeries.filter(function (novalista) {
+		return novalista.id == idSerie;
+	});
+
 
 	if (listaSeries.length == 0) {
-		serie.id = tamanho + 1;
-		listaSeries.push(serie);
+		novaSerie.id = tamanho + 1;
+		listaSeries.push(novaSerie);
 	} else {
-		serie.id = listaSeries[tamanho - 1].id + 1
-		listaSeries.push(serie);
+		novaSerie.id = listaSeries[tamanho - 1].id + 1
+		listaSeries.push(novaSerie);
 	}
 	res.send(listaSeries);
 });
