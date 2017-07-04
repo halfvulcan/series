e13307e9a6fcff68218fd6b05c1d5a83aa368012
Patricia <patricia.santana@metasix.com.br>
2017-07-04 20:45:45 -0300

    Refatorando o metodo push com a leitura de arquivos

--- src/routes/serie.js
+++ src/routes/serie.js
@@ -5,51 +5,56 @@ const express = require('express'),
 	fs = require("fs"),
 	arquivo = 'lista.json';
 
-fs.readFile(arquivo, function (err, lista) {
-	let listaSeries = [];
-	listaSeries = JSON.parse(lista);
+let listaSeries = fs.readFileSync(arquivo);
+listaSeries = JSON.parse(listaSeries);
 
-	router.get('/series', function (req, res) {
-		let serieFiltrada = [];
-		let filtroNome = req.query.nome || false;
-		let filtroCategoria = req.query.categoria || false;
+router.get('/series', function (req, res) {
+	let serieFiltrada = [];
+	let filtroNome = req.query.nome || false;
+	let filtroCategoria = req.query.categoria || false;
 
-		serieFiltrada = listaSeries.filter(function (serieFiltrada) {
-			if (filtroNome && filtroCategoria)
-				return filtroNome == serieFiltrada.nome && filtroCategoria == serieFiltrada.categoria;
-			else
-				return filtroNome == serieFiltrada.nome || filtroCategoria == serieFiltrada.categoria;
-		});
-
-		if (filtroNome || filtroCategoria) res.send(serieFiltrada);
-		else res.send(listaSeries);
+	serieFiltrada = listaSeries.filter(function (serieFiltrada) {
+		if (filtroNome && filtroCategoria)
+			return filtroNome == serieFiltrada.nome && filtroCategoria == serieFiltrada.categoria;
+		else
+			return filtroNome == serieFiltrada.nome || filtroCategoria == serieFiltrada.categoria;
 	});
 
-	router.get('/series/:id', function (req, res) {
-		let serieFiltrada = [];
-		let idSerie = req.params.id;
+	if (filtroNome || filtroCategoria) res.send(serieFiltrada);
+	else res.send(listaSeries);
+});
 
-		serieFiltrada = listaSeries.filter(function (serieFiltrada) {
-			return serieFiltrada.id == idSerie;
-		});
-		if (serieFiltrada.length > 0) res.send(serieFiltrada);
-		else res.status(404).send({ error: 'Id não encontrado' });
+router.get('/series/:id', function (req, res) {
+	let serieFiltrada = [];
+	let idSerie = req.params.id;
 
+	serieFiltrada = listaSeries.filter(function (serieFiltrada) {
+		return serieFiltrada.id == idSerie;
 	});
+	if (serieFiltrada.length > 0) res.send(serieFiltrada);
+	else res.status(404).send({ error: 'Id não encontrado' });
 
-	router.post('/series', function (req, res) {
-		let serie = req.body;
-		let novaSerie;
-		if (listaSeries.length > 0)
-			serie.id = listaSeries[listaSeries.length - 1].id + 1
-		else serie.id = 1;
-		listaSeries.push(serie);
-		novaSerie = JSON.stringify(listaSeries);
+});
 
-		fs.writeFile(arquivo, novaSerie, function (err) {
-			if (err) return console.error(err);
-		});
+router.post('/series', function (req, res) {
+	let serie = req.body;
+
+	if (listaSeries.length > 0)
+		serie.id = listaSeries[listaSeries.length - 1].id + 1
+	else serie.id = 1;
+
+	listaSeries.push(serie);
+	res.send(serie);
+	let novaSerie = JSON.stringify(listaSeries);
+
+	fs.writeFile(arquivo, novaSerie, function (err) {
+		if (err) return console.error(err);
 	});
 });
+
+
+
+
+
 
 module.exports = router;
