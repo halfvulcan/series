6eaf1c6b9ef08a504151b8979860e36dab04546e
Patricia <patricia.santana@metasix.com.br>
2017-07-04 19:22:09 -0300

    Refatorando o metodo que le o arquivo que contem a lista de series

--- src/routes/serie.js
+++ src/routes/serie.js
@@ -2,18 +2,18 @@
 const express = require('express'),
 	router = express.Router(),
 	app = require('../app'),
-	fs = require("fs");
+	fs = require("fs"),
+	arquivo = 'lista.json';
 
-let listaSeries = [];
+fs.readFile(arquivo, function (err, lista) {
+	let listaSeries = [];
+	listaSeries = JSON.parse(lista);
 
+	router.get('/series', function (req, res) {
+		let serieFiltrada = [];
+		let filtroNome = req.query.nome || false;
+		let filtroCategoria = req.query.categoria || false;
 
-router.get('/series', function (req, res) {
-	let serieFiltrada = [];
-	let filtroNome = req.query.nome || false;
-	let filtroCategoria = req.query.categoria || false;
-
-	fs.readFile('lista.json', function (err, lista) {
-		listaSeries = JSON.parse(lista);
 		serieFiltrada = listaSeries.filter(function (serieFiltrada) {
 			if (filtroNome && filtroCategoria)
 				return filtroNome == serieFiltrada.nome && filtroCategoria == serieFiltrada.categoria;
@@ -24,39 +24,29 @@ router.get('/series', function (req, res) {
 		if (filtroNome || filtroCategoria) res.send(serieFiltrada);
 		else res.send(listaSeries);
 	});
-});
 
+	router.get('/series/:id', function (req, res) {
+		let serieFiltrada = [];
+		let idSerie = req.params.id;
 
-
-router.get('/series/:id', function (req, res) {
-	let serieFiltrada = [];
-	let idSerie = req.params.id;
-
-	fs.readFile('lista.json', function (err, lista) {
-		listaSeries = JSON.parse(lista);
 		serieFiltrada = listaSeries.filter(function (serieFiltrada) {
 			return serieFiltrada.id == idSerie;
 		});
-
-		if (serieFiltrada.length > 0)
-			res.send(serieFiltrada);
+		if (serieFiltrada.length > 0) res.send(serieFiltrada);
 		else res.status(404).send({ error: 'Id não encontrado' });
+
 	});
-});
 
-router.post('/series', function (req, res) {
-	let serie = req.body;
-	let novaSerie;
-
-	fs.readFile('lista.json', function (err, lista) {
-		listaSeries = JSON.parse(lista);
+	router.post('/series', function (req, res) {
+		let serie = req.body;
+		let novaSerie;
 		if (listaSeries.length > 0)
 			serie.id = listaSeries[listaSeries.length - 1].id + 1
 		else serie.id = 1;
 		listaSeries.push(serie);
 		novaSerie = JSON.stringify(listaSeries);
 
-		fs.writeFile('lista.json', novaSerie, function (err) {
+		fs.writeFile(arquivo, novaSerie, function (err) {
 			if (err) return console.error(err);
 		});
 	});
