3992afe68c6912b0f6d1d9a66efafd4589ff9d6f
Patricia <patricia.santana@metasix.com.br>
2017-07-11 14:02:02 -0300

    Corrigindo a saida de erros do DAO

--- infra/serieDAO.js
+++ infra/serieDAO.js
@@ -4,27 +4,24 @@ const connection = require('./createDBConnection');
 const serieDAO = {
     listagem: function (callback) {
         connection.query("SELECT * FROM series", function (err, rows, fields) {
-            if (!err) {
-                callback(rows);
-            }
-            else
-                return 'Error while performing Query.';
+            if (err) throw err;
+            callback(rows);
         });
     },
     insere: function (serie) {
         connection.query('INSERT INTO series SET ?', serie, function (err, result) {
-            if (err) console.log(err);
+            if (err) throw err;
         });
     },
     atualiza: function (serie, idSerie) {
         connection.query('UPDATE series SET ? WHERE id = ?', [serie, idSerie], function (err, result) {
-            if (err) console.log(err);
+            if (err) throw err;
 
         });
     },
     deleta: function (idSerie) {
         connection.query('DELETE FROM series WHERE id = ?', idSerie, function (err, result) {
-            if (err) console.log(err);
+            if (err) throw err;
         });
     }
 
